0ea1d9dc9f6b156ff1fb333873bbbd8b1ac3809c
Priyaranjan6 <priyaranjan.hutechsolutions@gmail.com>
2021-04-16 13:38:08 +0530

    java programs without test cases

---
+++ src/com/hutech/javaprograms/CountPairs.java
@@ -0,0 +1,17 @@
+package com.hutech.javaprograms;
+
+public class CountPairs {
+
+	public static void main(String[] args) {
+		int[] i1 = { 8, 3, 1, 6, 4, 9, 7, 5, 11, 13 };
+		int count = 0;
+		for (int i = 0; i + 1 < i1.length; i++) {
+			int diff = i1[i] - i1[i + 1];
+			if (diff == 2 || diff == -2) {
+				int pair = count += 1;
+				System.out.print("Pair " + pair + "---> ");
+			}
+			System.out.println(i1[i] + "-" + i1[i + 1] + " = " + diff);
+		}
+	}
+}
---
+++ src/com/hutech/javaprograms/PrintLengthMoreThanFourChar.java
@@ -0,0 +1,20 @@
+package com.hutech.javaprograms;
+
+public class PrintLengthMoreThanFourChar {
+	
+/*	static void print() {
+		String[] s1= {"Ramesh","Aajy","Abhishek","Smita","Jay","Amit","Roja"};
+		for(int i=0;i<s1.length;i++) {
+			while(s1[i].length()>4) {
+				System.out.println(s1[i]+" ---- >"+s1[i].length());
+				break;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		//String[] s1= {"Ramesh","Aajy","Abhishek","Smita","Jay","Amit","Roja"};
+		print();
+	}*/
+
+}
